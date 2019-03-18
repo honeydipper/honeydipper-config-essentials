b073ef793fc3565a68794534065af12eba91f691
Chun Huang <charles.huang@joinhoney.com>
2019-03-18 15:09:02 -0700

    docs(notify): notify should send errors to notify_on_error

--- docs/classes/HoneydipperConfig/Essentials/Workflows/notify-SummaryToolTips.js
+++ docs/classes/HoneydipperConfig/Essentials/Workflows/notify-SummaryToolTips.js
@@ -1,1 +1,1 @@
-NDSummary.OnToolTipsLoaded("HoneydipperConfigClass:Essentials.Workflows.notify",{78:"<div class=\"NDToolTip TSection LHoneydipperConfig\"><div class=\"TTSummary\">This abstract workflow simplifies how you send message to chat.</div></div>"});
+NDSummary.OnToolTipsLoaded("HoneydipperConfigClass:Essentials.Workflows.notify",{56:"<div class=\"NDToolTip TSection LHoneydipperConfig\"><div class=\"TTSummary\">This abstract workflow simplifies how you send message to chat.</div></div>"});
