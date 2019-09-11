b66cb411bebfd5c8b03ba49fb5739261ee674bef
Chun Huang <charles.huang@joinhoney.com>
2019-09-11 14:51:28 -0700

    fix: correcting git-clone predefined step for run_kubernetes

--- kubernetes/resources/git-clone.sh
+++ kubernetes/resources/git-clone.sh
@@ -2,11 +2,11 @@ set -eo pipefail
 
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-echo >~/.ssh/config <<EOF
+cat >~/.ssh/config <<EOF
 Host *
   StrictHostKeyChecking no
   UserKnownHostsFile    /dev/null
   IdentityFile          /root/id_rsa
 EOF
 
-git clone --single-branch ${BRANCH:+--branch} $BRANCH $REPO $NAME
+git clone --single-branch ${BRANCH:+--branch} $BRANCH $REPO repo
